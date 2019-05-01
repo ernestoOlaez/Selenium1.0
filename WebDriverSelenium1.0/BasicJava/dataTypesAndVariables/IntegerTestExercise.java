4bf0b92c904d66d66a9fe6557b05a8d656ce3fb0
ernesto <olaez_ernesto@hotmail.com>
2019-05-01 15:54:26 -0500

    There are a new classes for operators package

---
+++ WebDriverSelenium1.0/BasicJava/dataTypesAndVariables/IntegerTestExercise.java
@@ -0,0 +1,25 @@
+package dataTypesAndVariables;
+
+public class IntegerTestExercise {
+
+	public static void main(String[] args)
+	{
+		//Use the int keyword to declare a integer variable
+				int carSpeed;
+				
+				// Use the integer variable with value 20
+				carSpeed=20;
+				
+				//Print the value of the integer variable
+				System.out.println("Car is running at the speed of: "+ carSpeed);
+				
+				//Change the value of integer variable
+				
+				carSpeed=carSpeed + 20;
+				
+				//Print the value of integer variable after changed
+				System.out.println("Car is running at the speed of: "+ carSpeed);
+
+	}
+
+}
