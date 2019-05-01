4aa504dee4d47f509b2c3ebfa9b4411ac7d69923
ernesto <olaez_ernesto@hotmail.com>
2019-05-01 16:51:14 -0500

    Add new decision Making classes

---
+++ WebDriverSelenium1.0/BasicJava/Switch_Statement.java
@@ -0,0 +1,35 @@
+
+public class Switch_Statement 
+{
+
+	public static void main(String[] args)
+	{
+		int iDay=5;
+		String sDay="Monday";
+		switch(iDay)
+		{
+			case 1:
+				System.out.printf("Today is Monday");
+				break;
+			case 2:
+				System.out.printf("Today is Tuesday");
+			break;
+			case 3:
+				System.out.printf("Today is Wednesday");
+			break;
+			case 4:
+				System.out.printf("Today is Thursday");
+				break;
+			case 5:
+				System.out.printf("Today is Friday");
+				break;
+			case 6:
+				System.out.printf("Today is Saturday");
+				break;
+			case 7:
+				System.out.printf("Today is Sunday");
+				break;
+		}
+	}
+
+}
