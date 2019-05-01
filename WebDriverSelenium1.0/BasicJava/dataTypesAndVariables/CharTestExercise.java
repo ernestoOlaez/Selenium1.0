8d0131141f11400a4751ec99af27c67c401460e2
ernesto <olaez_ernesto@hotmail.com>
2019-05-01 15:45:27 -0500

    New class

---
+++ WebDriverSelenium1.0/BasicJava/dataTypesAndVariables/CharTestExercise.java
@@ -0,0 +1,18 @@
+package dataTypesAndVariables;
+
+public class CharTestExercise {
+
+	public static void main(String[] args)
+	{
+		// Use the char keyword to declare character variable
+		char a;
+				
+		//Initialize the char variable with value 'p'
+		a='p';
+				
+		//Print the value of char variable
+		System.out.println("Value of char is: " + a);
+
+	}
+
+}
