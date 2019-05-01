6a1dcd696124ad4db936148dcbed81b2b3ee0a29
ernesto <olaez_ernesto@hotmail.com>
2019-05-01 13:27:19 -0500

    Create the Repository for WebDriver

---
+++ WebDriverSelenium1.0/BasicJava/dataTypesAndVariables/BooleanTestExcersice.java
@@ -0,0 +1,31 @@
+package dataTypesAndVariables;
+
+public class BooleanTestExcersice
+{
+
+	public static void main(String[] args)
+	{
+
+		//Use the boolean keyword to declare boolean value
+		boolean testResult;
+
+		//Initalize the boolean variable with true or false, once boolean declaration is done
+		testResult=true;
+
+		//Example
+		System.out.println("This is a program that use a variable boolean in JAVA.");
+		System.out.println("by: Ernesto Olaez.");
+
+		//Print the value of boolean variable
+		System.out.println("Test Result is: "+ testResult);
+
+		testResult=false;
+
+		//Print the value of boolean variable
+		System.out.println("Test Result is:"+ testResult);
+
+
+
+	}
+
+}
